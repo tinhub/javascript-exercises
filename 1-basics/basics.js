9f8a8697258d8dbd9054dc1bab0dbf3a88511bb9
Martin Ihugo <muniumartinihugo@gmail.com>
2019-10-24 17:07:40 +0300

    Deleted a property from an object

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -173,7 +173,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            delete object.someProperty
             ////////////////////////////////////////////////////////////////////
 
             // object.someProperty is undefined...
