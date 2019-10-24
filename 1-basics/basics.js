2f28ae303a484ed21730c7a873475fdab8ae850f
Martin Ihugo <muniumartinihugo@gmail.com>
2019-10-24 22:53:47 +0300

    Created an array with an initial length

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -233,7 +233,7 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
             // ✏️ ADD CODE HERE ✏️
-
+            const longArray =Array(100)
             ////////////////////////////////////////////////////////////////////
 
             expect(longArray).to.be.instanceOf(Array);
