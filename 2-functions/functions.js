f9e5d3b5df9b3c60ad22000b7ebc85788d3a2388
MARTIN <muniumartinihugo@gmail.com>
2019-11-19 09:23:22 -0800

    Storing the number of arguments

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -364,7 +364,8 @@ describe("functions", function() {
                 // Uh-oh, the function 'breakTheLoop' calls itself recursively (see below).
                 // There are two possible solutions that break the loop. Can you find both?
                 // ✏️ ADD CODE HERE ✏️
-
+                //return;
+                function breakTheLoop(){};
                 ////////////////////////////////////////////////////////////////////
 
                 breakTheLoop(); // Recursive call
@@ -380,7 +381,13 @@ describe("functions", function() {
             // Can you implement a function 'myFunction' that returns a function
             // which returns the number of arguments of 'myFunction' when called?
             // ✏️ ADD CODE HERE ✏️
+            function myFunction(){
+                 const length = arguments.length;
 
+                return function(){
+                    return length;
+                };
+            }
             ////////////////////////////////////////////////////////////////////
 
             // Look at that strange code: myFunction()().
