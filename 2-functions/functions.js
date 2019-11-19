95c5886fed51c51711f74e52ace2f8b18ee30681
MARTIN <muniumartinihugo@gmail.com>
2019-11-19 10:11:21 -0800

    described Lost context

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -529,7 +529,7 @@ describe("functions", function() {
         });
     });
 
-    describe("Implementing map()", () => {
+    describe("()", () => {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you implement a function named 'map' that:
@@ -639,7 +639,7 @@ describe("functions", function() {
 
             // Now we're registering the service.handleEvent method so that
             // it will be called as soon as the event is emitted.
-            eventEmitter.addEventListener(service.handleEvent);
+            eventEmitter.addEventListener(service.handleEvent.bind(service));
             // Now let's emit the event. However, this will fail.
             // Find the reason why and fix it.
             eventEmitter.emit();
